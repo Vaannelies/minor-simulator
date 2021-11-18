82470d7aadb031c833f38bd678125b1fb5d20f61
Hamza Mehmood <hmehmood@hotmail.com>
2021-11-18 13:58:44 +0100

    Remove neural network from lidar_pilot_world

--- simulations/car/neural_network.py
+++ simulations/car/neural_network.py
@@ -55,6 +55,7 @@ with open('.\data\samples.dat') as sampleFile:
 # data = shuffle(df.to_numpy())
 print('data', data)
 
+
 # Remove duplicates from data
 # print('Original excel data length:', len(data))
 # filteredData =np.unique(data,axis=0)
@@ -80,23 +81,46 @@ x = xColumns
 y = yColumn 
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 1)
 
-# Use MinMaxScaler on x_train and x_test
-# scaler = MinMaxScaler()
-# scaler.fit(x_train,y_train)
-# x_train = scaler.transform(x_train)
-# x_test = scaler.transform(x_test)
+x_train = np.array(x_train)
+y_train = np.array(y_train).reshape(-1,1)
+x_test = np.array(x_test)
+y_test = np.array(y_test).reshape(-1,1)
+print('x_train', x_train.shape)
+print('y_train', y_train.shape)
 
+
+# Use MinMaxScaler on x_train and x_test
+scalerX = MinMaxScaler(feature_range = (-1, 1), copy = True, clip = False)
+scalerX.fit(x_train)
+scaled_x_train = scalerX.transform(x_train)
+scaled_x_test = scalerX.transform(x_test)
+
+scalerY = MinMaxScaler(feature_range = (-1, 1), copy = True, clip = False)
+scalerY.fit(y_train)
+scaled_y_train = scalerY.transform(y_train)
+scaled_y_test = scalerY.transform(y_test)
+
+# predictedY = predict(test_x)
+# predictedY = scalerY.inverse_transform(predictedY)
+# Voordat je predict gaat gebruiken moet je die input data dus ook elke keer op dezelfde manier scalen. Anders krijgt ie bij predict()
+# hele andere input mee dan hij gewend is bij het trainen (waarbij je het wel gescaled hebt).
+print('2222')
 regr = MLPRegressor(
     hidden_layer_sizes=(64,64,64),
     activation="logistic",
     random_state = 1, 
-    max_iter = 2048, ).fit(x_train, y_train)
-# regr.add(layers.Dense(10, activation='relu'))
+    max_iter = 2048, 
+    ).fit(x_train, y_train.reshape(-1,))
+    # ).fit(scaled_x_train, scaled_y_train.reshape(-1,)) // with scale
 
 right_answers = 0
-test_answers = regr.predict(x_test)
+test_answers = regr.predict(x_test).reshape(-1,1)
+# test_answers = regr.predict(scaled_x_test).reshape(-1,1) // with scale
+print('test_answers', test_answers.shape)
+# test_answers = scalerY.inverse_transform(test_answers) // with scale
+
 for i in range(len(x_test)):
-    print('input nodes', x_test[i], 'predicted answer (output node)', '{:f}'.format(test_answers[i]), 'real answer', y_test[i])
+    print('input nodes', x_test[i], 'predicted answer (output node)', list(map('{:.2f}'.format,test_answers[i])), 'real answer', y_test[i])
     if(y_test[i] - test_answers[i]) < 1 and (y_test[i] - test_answers[i]) > -2:
         print('Right!')
         print('Difference:', y_test[i] - test_answers[i])
@@ -105,21 +129,10 @@ for i in range(len(x_test)):
     if(y_test[i] - test_answers[i]) < 1 and (y_test[i] - test_answers[i]) > -2:
         right_answers += 1
 
-print()                     # wat betekent dit 
-            #                           v
-# print('predict', regr.predict(x_test[:2])) # <-----
+print()                         
 print('Score', regr.score(x_test, y_test))
+# print('Score', regr.score(scaled_x_test, scaled_y_test)) // with scale
 print('Total test data:', len(x_test), '\t',  'Right answers (difference less than 2):', right_answers)
-
-# print('The \'score\' is the coefficient of determination of the prediction. A.k.a. de \'determinatiecoëfficiënt\'.')
-# print('De determinatiecoëfficiënt is een heel ingewikkeld iets op wikipedia.')
-
-# X = np.array(df)
-# print('X =', X)
-
-# neural_net = MLPRegressor().fit(X[:, :-1], X[:,-1])
-# res = neural_net.predict([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
-# print('result =', res)
 
 # Deze line uitcommenten als je de getrained model wilt opslaan in een aparte file
 # joblib.dump(regr, './data/trained_network.sav') 
